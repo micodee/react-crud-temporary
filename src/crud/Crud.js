d58910f1c0a412cbebec699286c39f5e460de676
micodee <martomhidayat>
2022-12-02 09:24:52 +0700

    Set State kosong form tambah data

--- src/crud/Crud.js
+++ src/crud/Crud.js
@@ -10,34 +10,42 @@ export default class Crud extends Component {
     this.state = {
       makanans: [],
       nama: "",
-      harga: 0,
+      harga: "",
       deskripsi: "",
       id: "",
     };
   }
 
-//   mengubah data state atau menginput ke data state 
+  //   mengubah data state atau menginput ke data state
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
     });
   };
 
-//   menghilangkan isian seperti reload
+  //   menghilangkan isian seperti reload
   handleSubmit = (event) => {
     event.preventDefault();
     // console.log('halo everybody', this.state)
     this.setState({
-        makanans: [
-            ...this.state.makanans,
-            {
-                nama : this.state.nama,
-                harga : this.state.harga,
-                deskripsi : this.state.deskripsi,
-                id: this.state.makanans.length+1,
-            }
-        ]
-    })
+      makanans: [
+        ...this.state.makanans,
+        {
+          nama: this.state.nama,
+          harga: this.state.harga,
+          deskripsi: this.state.deskripsi,
+          id: this.state.makanans.length + 1,
+        },
+      ],
+    });
+
+    // membuat data menjadi kosong
+    this.setState({
+      nama: "",
+      harga: "",
+      deskripsi: "",
+      id: "",
+    });
   };
 
   render() {
@@ -47,8 +55,12 @@ export default class Crud extends Component {
         <ListNavbar />
         <div className="container-sm mt-4">
           <h2>CRUD REACT JS</h2>
-          <ListTable makanans={this.state.makanans}/>
-          <ListForm {...this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+          <ListTable makanans={this.state.makanans} />
+          <ListForm
+            {...this.state}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+          />
         </div>
       </div>
     );
