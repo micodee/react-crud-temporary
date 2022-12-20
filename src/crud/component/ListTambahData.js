fe164526f072eb0f06d3d6c1fc837e2600b30032
micodee <martomhidayat>
2022-12-20 12:57:55 +0700

    add required

--- src/crud/component/ListTambahData.js
+++ src/crud/component/ListTambahData.js
@@ -17,19 +17,19 @@ const ListForm = ({nama, harga, deskripsi, handleChange, handleSubmit, id }) => 
             <Form.Group className="mb-3" controlId="nama-makanan">
               <Form.Label>Nama Makanan</Form.Label>
               <Form.Control type="text" name="nama" placeholder="Masukan nama makanan" 
-              value={nama} onChange={(event) => handleChange(event)} />
+              value={nama} onChange={(event) => handleChange(event)} required />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="harga-makanan">
               <Form.Label>Harga</Form.Label>
               <Form.Control type="number" name="harga" placeholder="Masukan harga makanan" 
-              value={harga} onChange={(event) => handleChange(event)} />
+              value={harga} onChange={(event) => handleChange(event)} required />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="deskripsi-makanan">
               <Form.Label>Deskripsi</Form.Label>
               <Form.Control as="textarea" name="deskripsi" rows={3} placeholder="Masukan deskripsi makanan" 
-              value={deskripsi} onChange={(event) => handleChange(event)}/>
+              value={deskripsi} onChange={(event) => handleChange(event)} required />
             </Form.Group>
 
             <Button variant="primary" type="submit">
